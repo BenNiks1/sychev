8289ef352135ed9cf7d7faf2e8e4660919d997c2
BenNiks1 <ikke.trodde@gmail.com>
2021-03-02 19:50:52 +0300

    canvas

---
+++ src/components/ArcadeCanvas/Canvas.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import {
+  htmlImg,
+  pugImg,
+  bemImg,
+  cssImg,
+  sassImg,
+  gulpImg,
+  jsImg,
+  reactImg,
+} from "./images";
+
+const Canvas = () => {
+  const canvasRef = React.useRef(null);
+
+  const canvas = () => {
+    // workaround for importing pictures
+    const htmlPng = new Image(),
+      pugPng = new Image(),
+      bemPng = new Image(),
+      cssPng = new Image(),
+      sassPng = new Image(),
+      gulpPng = new Image(),
+      jsPng = new Image(),
+      reactPng = new Image();
+    htmlPng.src = htmlImg;
+    pugPng.src = pugImg;
+    bemPng.src = bemImg;
+    cssPng.src = cssImg;
+    sassPng.src = sassImg;
+    gulpPng.src = gulpImg;
+    jsPng.src = jsImg;
+    reactPng.src = reactImg;
+    // Canvas
+    let ctx = canvasRef.current.getContext("2d"),
+      canvasWidth = (canvasRef.current.width = 530),
+      canvasHeight = (canvasRef.current.height = 400),
+      // Text color, font, background color
+      // textColor = ["black"];
+
+      fillText = [
+        htmlPng,
+        pugPng,
+        bemPng,
+        cssPng,
+        sassPng,
+        gulpPng,
+        jsPng,
+        reactPng,
+      ],
+      textFont = " bold 50px yahei",
+      backgroundColor = "rgb(255,255,255,0)",
+      // Size of Background Rectangle
+      backgroundWidth = 150,
+      backgroundHeight = 150,
+      // When adding text to a rectangle, the height deviates a little.
+      fixHeight = 150,
+      // Speed
+      speedX = 2,
+      speedY = 2,
+      // Moving direction, starting at'r-b'lower right
+      direction = "r-b",
+      // icon X and Y coordinates
+      positionX = 0,
+      positionY = 0,
+      // Number of collisions, used to calculate background and text color
+      count = 0;
+
+    html();
+    function html() {
+      // Moving direction
+      switch (direction) {
+        // Lower right
+        case "r-b":
+          positionX += speedX;
+          positionY += speedY;
+          break;
+        // Upper right
+        case "r-t":
+          positionX += speedX;
+          positionY -= speedY;
+          break;
+        // Lower left
+        case "l-b":
+          positionX -= speedX;
+          positionY += speedY;
+          break;
+        // Upper left
+        case "l-t":
+          positionX -= speedX;
+          positionY -= speedY;
+          break;
+      }
+      // Empty the canvas
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      // Redraw
+      ctx.fillRect(positionX, positionY, backgroundWidth, backgroundHeight);
+      // Collision detection
+      // Bottom
+      if (positionY + backgroundHeight >= canvasHeight) {
+        direction = direction.replace("b", "t");
+        // Collision Number Statistics
+        count += 1;
+      }
+      // Right
+      if (positionX + backgroundWidth >= canvasWidth) {
+        direction = direction.replace("r", "l");
+        count += 1;
+      }
+      // Left
+      if (positionX < 0) {
+        direction = direction.replace("l", "r");
+        count += 1;
+      }
+      // Upper
+      if (positionY < 0) {
+        direction = direction.replace("t", "b");
+        count += 1;
+      }
+      // Text
+      ctx.font = textFont;
+      // ctx.fillStyle = textColor;
+
+      ctx.drawImage(
+        fillText[count % 8],
+        positionX,
+        positionY + backgroundHeight - fixHeight
+      );
+      // ctx.fillText(
+      //   fillText[count % 8],
+      //   positionX,
+      //   positionY + backgroundHeight - fixHeight
+      // );
+      // Background color
+      ctx.fillStyle = backgroundColor;
+      // Start animation
+      window.requestAnimationFrame(html);
+    }
+  };
+  React.useEffect(() => {
+    canvas();
+  }, []);
+  return (
+    <React.Fragment>
+      <canvas className="canvas" ref={canvasRef}></canvas>
+    </React.Fragment>
+  );
+};
+
+export default Canvas;
--- src/components/ArcadeCanvas/index.jsx
+++ src/components/ArcadeCanvas/index.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import arcadeImg from '../../assets/img/arcade.png'
+import React from "react";
+import arcadeImg from "../../assets/img/arcade.png";
+import Canvas from "./Canvas";
 
 const ArcadeCanvas = () => {
   return (
     <div className="main__arcade">
-        <div className="main__arcade-box">
-          <img src={arcadeImg} alt="arcade" className="main__arcade-img" />
-        </div>
-        <div className="main__arcade-canvas">
-          <canvas className="canvas" id="canvas"></canvas>
-        </div>
+      <div className="main__arcade-box">
+        <img src={arcadeImg} alt="arcade" className="main__arcade-img" />
       </div>
-  )
-}
+      <div className="main__arcade-canvas">
+        <Canvas />
+      </div>
+    </div>
+  );
+};
 
-export default ArcadeCanvas
+export default ArcadeCanvas;
